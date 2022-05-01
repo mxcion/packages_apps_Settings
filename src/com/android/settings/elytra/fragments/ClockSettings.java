Project-Awaken/android_packages_apps_Settings

Settings: Move clock settings to display customizations fragment
* Also add missing summary
* Re-arrange display customization fragement while we're at it

Change-Id: I90939cbc9a3956c4ef8ffe1c8c5b33cc2aecff88
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

src/com/android/settings/awaken/fragments/ClockSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.datetime;
+package com.android.settings.awaken.fragments;
 
 import android.app.AlertDialog;
 import android.content.ContentResolver;

mxcion/packages_apps_Settings

src/com/android/settings/elytra/fragments/ClockSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.datetime;
+package com.android.settings.elytra.fragments;
 
 import android.app.AlertDialog;
 import android.content.ContentResolver;